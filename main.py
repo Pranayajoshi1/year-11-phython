af379ce6e3882dc18694e9fc7356420b6f2f6e71
Pranaya Joshi WSC <joiship@wsc.school.nz>
2024-05-02 12:26:52 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+print("hello")
+# Asking for  the user name
+name = input("What's your name?")
+# Greet the user  introduce the quizb
+print("Welcome to the quiz.",name)
+print("We are going to play quiz, so be ready for that.")
+# Ask the user a question 
+answer = input("What's the capital city of New Zealand?,")
+# Tell them the correct answer 
+if answer == "Welligiton":
+    print("Correct!")
+print("Question no 2")
+# Ask the user a question 
+input("Who wrote the book Chitty-Chitty-Bang-Bang: The Magical Car?")
+# Tell them the correct answer 
+print("Ian Fleming is the right of question 2")
+print("Question no 3")
+# Ask the user a question 
+input("In which part of your body would you find the cruciate ligament?")
+# Tell them the correct answer 
+print("Kneeis the right of question 3")
+# Game end
+print(" Game end. Thank you for playing")
